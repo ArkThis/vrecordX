fe776b6770bcaf50dc8087d1249f745fa0094efd
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-01-18 13:24:52 +0100

    fixed missing "PWD" in HDV script.

--- hdv.sh
+++ hdv.sh
@@ -19,6 +19,7 @@ ARCHIVE_SIGNATURE=${ARCHIVE_SIGNATURE,,}                        # Force archive 
 #CMD_HDV_MASK="$DVGRAB -f hdv --size 0 -showstatus %s - | ffplay -f mpegts -x 640 -y 360 -"
 CMD_HDV_MASK="$DVGRAB -f hdv --size 0 -showstatus %s/%s- - | ffplay -f mpegts -x 1280 -y 720 -"
 
+PWD=$(pwd)
 CMD=$(printf "$CMD_HDV_MASK" "$PWD" "$ARCHIVE_SIGNATURE")
 $ZENITY --info --text="Capturing HDV with this command:\n $CMD" --width=500
 
