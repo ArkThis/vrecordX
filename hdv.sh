8457befaa0961ed1fefb2cd4c7b508602784af19
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-01-18 13:22:37 +0100

    Added working directory to HDV script.

--- hdv.sh
+++ hdv.sh
@@ -12,15 +12,14 @@ DVGRAB="dvgrab"
 
 
 if ! ARCHIVE_SIGNATURE=$($ZENITY --entry --text "Enter archive signature:" --title "HDV Ingest"); then
-       exit;
+           exit;
 fi
 ARCHIVE_SIGNATURE=${ARCHIVE_SIGNATURE,,}                        # Force archive signature to lowercase
 
 #CMD_HDV_MASK="$DVGRAB -f hdv --size 0 -showstatus %s - | ffplay -f mpegts -x 640 -y 360 -"
-CMD_HDV_MASK="$DVGRAB -f hdv --size 0 -showstatus %s - | ffplay -f mpegts -x 1280 -y 720 -"
+CMD_HDV_MASK="$DVGRAB -f hdv --size 0 -showstatus %s/%s- - | ffplay -f mpegts -x 1280 -y 720 -"
 
-NAME="$1"
-CMD=$(printf "$CMD_HDV_MASK" "$NAME")
+CMD=$(printf "$CMD_HDV_MASK" "$PWD" "$ARCHIVE_SIGNATURE")
 $ZENITY --info --text="Capturing HDV with this command:\n $CMD" --width=500
 
 eval "$CMD"
@@ -31,5 +30,4 @@ $DVCONT rewind
 # TODO: Wait for rewind to finish, before ejecting (status = idle?)
 STATUS=$($DVCONT status)
 echo "$STATUS"
-# dvcont eject
-
+# $DVCONT eject
