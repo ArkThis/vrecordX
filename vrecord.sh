6734ad1961bb914a011af72b1b5986f8bcc4d271
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-01-19 17:34:18 +0100

    Added wrapper to keep console open when vrecord exists (for debugging)

---
+++ vrecord.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a (very) thin wrapper that does nothing else, but keeping open the
+# shell after vrecord has exited (for whatever reason).
+# This is handy to see possible errors/issues, even when being called from a
+# Linux Desktop Launcher, for example.
+
+./vrecord
+
+read -p "Press any key to continue."
+clear
