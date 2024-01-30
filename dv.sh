79ecf29174be63c957f497dffa78be179221adbc
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-01-30 16:38:59 +0100

    Added script for dvgrab DV capture alternative (testing).

---
+++ dv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# @description:
+#   Small script to make DV-capture more convenient.
+
+# @history:
+#   2024-01-25  peter_b     - Started.
+
+# Programs used:
+ZENITY="zenity"
+DVCONT="dvcont"
+DVGRAB="dvgrab"
+FFMPEG="ffmpeg"
+
+
+if ! ARCHIVE_SIGNATURE=$($ZENITY --entry --text "Enter archive signature:" --title "HDV Ingest"); then
+           exit;
+fi
+ARCHIVE_SIGNATURE=${ARCHIVE_SIGNATURE,,}                        # Force archive signature to lowercase
+
+CMD_DV_MASK="$DVGRAB -f dv1 -size 0 -showstatus %s/%s-.dv - | ffplay -f dv -"
+
+PWD=$(pwd)
+CMD=$(printf "$CMD_DV_MASK" "$PWD" "$ARCHIVE_SIGNATURE")
+$ZENITY --info --text="Capturing DV with this command:\n $CMD" --width=500
+
+eval "$CMD"
+
+# Rewind tape after capture:
+# $DVCONT rewind
+
+
+# TODO: Wait for rewind to finish, before ejecting (status = idle?)
+STATUS=$($DVCONT status)
+echo "$STATUS"
+# $DVCONT eject
+
