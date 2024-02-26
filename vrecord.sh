6e168531b8b79b1ba461a298da1f0c2e38813ae1
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-02-26 22:47:40 +0100

    Improved launchers+startup helper to support overriding record folder.

--- vrecord.sh
+++ vrecord.sh
@@ -5,6 +5,7 @@
 #   1. keeping the shell open after vrecord has exited (for whatever reason).
 #      This is handy to see possible errors/issues, even when being called from a
 #      Linux Desktop Launcher, for example.
+#      This is OPTIONAL, and can be enabled by using the '-w' (wait) parameter.
 #
 #   2. Ask for a recording IDENTIFIER (used as "Name of Recording" in vrecord).
 #      If an IDENTIFIER is provided, vrecord will record directly - and *not* start the GUI.
@@ -14,18 +15,40 @@
 #   3. Adds simple support for providing a recording directory path on-the-fly,
 #      overriding the value stored in vrecord's config_file.
 
-DIR_RECORD="$1"
 ZENITY="zenity"
 
+# Ask user for IDENTIFIER string.
 # HINT: Closing this input window with "Cancel", or entering an empty value,
 # enters the vrecord GUI normally.
 IDENTIFIER=$($ZENITY --width=400 --entry --text "Enter recording identifier:" --title "vrecord 'Name of Recording'")
 IDENTIFIER=${IDENTIFIER,,}                  # Force identifier to lowercase
 
-./vrecord -d "$DIR_RECORD/$IDENTIFIER" $IDENTIFIER
+ARGS=""
+
+# Check commandline parameters:
+while getopts "d:w" opt; do
+    case $opt in
+        d)
+            DIR_RECORD="${OPTARG}"
+            echo "Recording base: '$DIR_RECORD'"
+            ARGS="-d \"$DIR_RECORD/$IDENTIFIER\""
+            ;;
+        w)
+            WAIT="wait"
+            echo "Enabling wait."
+            ;;
+        ?)
+            echo "Invalid option: -${OPTARG}."
+            exit 1
+            ;;
+    esac
+done
+
+
+./vrecord $ARGS $IDENTIFIER
 
 # This keeps the terminal open, after vrecord has closed:
-if [ "$2" != "direct" ]; then
+if [ "$WAIT" == "wait" ]; then
     read -p "Press ENTER key to continue."
     clear
 fi
