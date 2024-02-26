0ef34be1471a1d2bfaf09b8ad266b24fc4cca781
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-02-26 22:23:47 +0100

    Added support for recording directory override to helper script.

--- vrecord.sh
+++ vrecord.sh
@@ -9,17 +9,23 @@
 #   2. Ask for a recording IDENTIFIER (used as "Name of Recording" in vrecord).
 #      If an IDENTIFIER is provided, vrecord will record directly - and *not* start the GUI.
 #      If no IDENTIFIER is given (or "Cancel" button pressed), vrecord GUI is started as normally.
+#      The IDENTIFIER is used as sub-foldername to the recording folder.
+#
+#   3. Adds simple support for providing a recording directory path on-the-fly,
+#      overriding the value stored in vrecord's config_file.
 
+DIR_RECORD="$1"
 ZENITY="zenity"
 
-# Closing this input window with "Cancel" 
+# HINT: Closing this input window with "Cancel", or entering an empty value,
+# enters the vrecord GUI normally.
 IDENTIFIER=$($ZENITY --width=400 --entry --text "Enter recording identifier:" --title "vrecord 'Name of Recording'")
 IDENTIFIER=${IDENTIFIER,,}                  # Force identifier to lowercase
 
-./vrecord $IDENTIFIER
+./vrecord -d "$DIR_RECORD/$IDENTIFIER" $IDENTIFIER
 
 # This keeps the terminal open, after vrecord has closed:
-if [ "$1" != "direct" ]; then
+if [ "$2" != "direct" ]; then
     read -p "Press ENTER key to continue."
     clear
 fi
