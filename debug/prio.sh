c8d0024a27b36e792133d474bec992adf0c803a5
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2025-05-08 12:38:06 +0200

    Added quick hack script to change ffmpeg scheduling priority (FIFO=99)

---
+++ debug/prio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set scheduling priority of capture process high(er)
+
+FFMPEG_BIN="ffmpeg-dl"
+PRIO=99     # 1=lowest, 99=highest priority
+
+echo "Updating scheduling priority for '$FFMPEG_BIN' to '$PRIO'"
+PID=$(pgrep $FFMPEG_BIN)
+
+if [ -z $PID ]; then
+    echo "ERROR: process '$FFMPEG_BIN' not found. is it running?"
+    ps aux | grep $FFMPEG_BIN
+    exit 1
+fi
+
+read -p "Press any key to update process (pid=$PID)"
+
+CMD="sudo chrt -f -p $PRIO $PID"
+echo "$CMD"
+eval "$CMD"
+
+# Show Updated/current scheduling:
+chrt -p $PID
